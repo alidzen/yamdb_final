b43b85485149260b23bf1a1cee01cc2eb5bffe04
alidzen <d.s.golubtsov@gmail.com>
2021-02-23 18:49:28 +0100

    [sp19] Remove port in web config

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -86,8 +86,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/', '/redoc'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'redoc'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
